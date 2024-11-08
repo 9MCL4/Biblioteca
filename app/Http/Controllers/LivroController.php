5f6ad1569fd313d262db7e5cd0fa154780c7998c
Michael Damacena De Assunção <23433@etehort.ps.sp>
2024-11-08 20:19:30 -0300

    first commit

--- app/Http/Controllers/LivroController.php
+++ app/Http/Controllers/LivroController.php
@@ -1,10 +1,29 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Livro;
 
 class LivroController extends Controller
 {
-    //
+    public function index(Request $request){
+    $Livros = Livro::all();
+        return view('Livro.Index', compact('livros'));
+}
+public function create(){
+    return view('livros.create');
+}
+
+public function store (Request $request){
+    $request->validade([
+'titulo' => 'required|max:100',
+'autor' => 'required|max:50',
+'ano_publicacao' => 'required|integer',
+'resumo' => 'nullable|string',
+    ]);
+    
+    Livro::create($request->all());
+
+    return redirect ()->route('Livro.index')->with('sucess','Registro criado com sucesso');
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controlers\LivroControler;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/livros', [LivroController::class, 'index'])->name('livros.index');
+Route::get('/livros/adicionar', [LivroController::class, 'create'])->name('livros.create');
+Route::post('/livros', [LivroController::class, 'store'])->name('livros.store');
